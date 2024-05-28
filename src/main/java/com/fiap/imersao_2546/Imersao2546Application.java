9d66e045ecfd55d0be754d00b25e3786e4247903
felipecosta <fcosta.paltrinieri@gmail.com>
2024-05-27 21:40:52 -0300

    Commit inicial

---
+++ src/main/java/com/fiap/imersao_2546/Imersao2546Application.java
@@ -0,0 +1,13 @@
+package com.fiap.imersao_2546;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Imersao2546Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Imersao2546Application.class, args);
+	}
+
+}
